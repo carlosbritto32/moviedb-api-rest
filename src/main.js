aa50b9bd17cc8e4784cc61749e817be5f14b2a63
CarlosBritto32 <carlosbritto32@gmail.com>
2022-09-07 16:56:00 -0500

    adding lazyloading to movies images and bug fixing null images apparence

--- src/main.js
+++ src/main.js
@@ -15,7 +15,17 @@ const searchMovie = "/search/movie";
 const searchMovieById = "/movie/";
 
 // UTILS
-function createMovies(movies, container) {
+
+const lazyLoader = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (!!entry.isIntersecting) {
+      const url = entry.target.getAttribute("data-img");
+      entry.target.setAttribute("src", url);
+    }
+  });
+});
+
+function createMovies(movies, container, lazyLoad = false) {
   container.innerHTML = "";
   movies.forEach((movie) => {
     const movieContainer = document.createElement("div");
@@ -26,12 +36,21 @@ function createMovies(movies, container) {
 
     const movieImg = document.createElement("img");
     movieImg.classList.add("movie-img");
-
     movieImg.setAttribute(
-      "src",
+      lazyLoad ? "data-img" : "src",
       `https://image.tmdb.org/t/p/w300/${movie.poster_path}`
     );
     movieImg.setAttribute("alt", movie.title);
+    movieImg.addEventListener("error", () => {
+      movieImg.setAttribute(
+        "src",
+        "https://static.platzi.com/static/images/error/img404.png"
+      );
+    });
+
+    if (lazyLoad) {
+      lazyLoader.observe(movieImg);
+    }
 
     movieContainer.appendChild(movieImg);
     container.appendChild(movieContainer);
@@ -62,8 +81,9 @@ function createCategories(categories, container) {
 async function getTrendingMoviesPreview() {
   const { data } = await api(`${trending}`);
   const movies = data.results;
+  console.log(movies);
 
-  createMovies(movies, trendingMoviesPreviewList);
+  createMovies(movies, trendingMoviesPreviewList, true);
 }
 
 async function getCategoriesPreview() {
@@ -81,7 +101,7 @@ async function getMoviesByCategory(id) {
   });
   const movies = data.results;
 
-  createMovies(movies, genericSection);
+  createMovies(movies, genericSection, true);
 }
 
 async function getMoviesBySearch(query) {
